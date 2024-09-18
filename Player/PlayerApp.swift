fe574045ed468e74ba51f1d01fa0ebbd84821c6b
Murtazali <m.magomedov.a@mail.ru>
2024-09-18 21:19:58 +0300

    Initial Commit

---
+++ Player/PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayerApp.swift
+//  Player
+//
+//  Created by Муртазали Магомедов on 18.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
